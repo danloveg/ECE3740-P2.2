20b52ca2443ff112bc8d925092ec498c3b492798
Daniel Lovegrove <d.lovegrove11@gmail.com>
2017-10-15 21:10:13 -0500

    Add command handling scheme

--- Client/src/clientcommandhandler/CommandHandler.java
+++ Client/src/clientcommandhandler/CommandHandler.java
@@ -1,5 +1,50 @@
 package clientcommandhandler;
 
+/**
+ * Handles the executing of the user's commands.
+ * 
+ * @author Daniel Lovegrove
+ */
 public class CommandHandler {
     
+    private final userinterface.StandardIO console;
+    private client.Client myClient;
+    
+    
+    /**
+     * Creates an instance with an associated user interface and client.
+     * @param console The console user interface
+     * @param client The client
+     */
+    public CommandHandler(userinterface.StandardIO console, client.Client client) {
+        this.console = console;
+        this.myClient = client;
+    }
+    
+    
+    /**
+     * Parses the user's command and executes the appropriate task.
+     * @param userCommand The string the user entered
+     */
+    public void execute(String userCommand) {
+        // First, trim any whitespace
+        String cmd = userCommand.trim();
+
+        switch (cmd) {
+            case "connect":
+                console.log("WIP: User wants to connect.");
+                break;
+            case "disconnect":
+                console.log("WIP: User wants to disconnect.");
+                break;
+            case "quit":
+                console.log("WIP: User wants to quit.");
+                break;
+            case "time":
+                console.log("WIP: User wants the time.");
+                break;
+            default:
+                break;
+        }
+    }
 }
